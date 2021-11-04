d8b061ead1aac92213172fe8354db48ce3198a09
shahid <shahid.ice.nstu@gmail.com>
2021-11-04 12:41:30 +0600

    Add models: YearModel, MonthModel, ReportModel app: monthly_repoort

--- monthly_report/models.py
+++ monthly_report/models.py
@@ -1,3 +1,57 @@
+# monthly_report/models.py
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class YearModel(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    year = models.PositiveSmallIntegerField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"year: {self.year}"
+
+
+class MonthModel(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    month = models.PositiveSmallIntegerField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"month: {self.month}"
+
+
+class ReportModel(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    year = models.ForeignKey(
+        YearModel, related_name="reports_full_year", on_delete=models.CASCADE
+    )
+    month = models.ForeignKey(
+        MonthModel, related_name="reports", on_delete=models.CASCADE
+    )
+    total_office = models.PositiveIntegerField(blank=True, null=True)
+    nispotti_krito_nothi = models.PositiveIntegerField(blank=True, null=True)
+    total_upokarvogi = models.PositiveIntegerField(blank=True, null=True)
+    total_users = models.PositiveIntegerField(blank=True, null=True)
+    total_nothi_users_male = models.PositiveIntegerField(blank=True, null=True)
+    total_nothi_users_female = models.PositiveIntegerField(blank=True, null=True)
+    total_mobile_app_users = models.PositiveIntegerField(blank=True, null=True)
+    total_nisponno = models.PositiveIntegerField(blank=True, null=True)
+    total_potrojari = models.PositiveIntegerField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['year', 'month'], name='year_month')
+        ]
+
+    def __str__(self):
+        return f"{self.year}, {self.month}"
