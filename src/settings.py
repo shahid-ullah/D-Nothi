135b70b019f51f6aa86e63b9cd0e7c65991cf3cb
shahid <shahid.ice.nstu@gmail.com>
2021-11-24 15:42:43 +0600

    turn off debug

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(env_file)
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
