deb5a574d8403341688320bc0e8c78058e3f76a0
shahid <shahid.ice.nstu@gmail.com>
2022-01-17 11:51:10 +0600

    register models to admin.py models: TableNameModel, ReportStorageModel'

--- monthly_report/admin.py
+++ monthly_report/admin.py
@@ -2,10 +2,13 @@
 from django.contrib import admin
 
 from .models import (GeneralDrilldownJSONDataModel, MonthModel, ReportModel,
-                     ReportTypeModel, YearModel)
+                     ReportStorageModel, ReportTypeModel, TableNameModel,
+                     YearModel)
 
 admin.site.register(YearModel)
 admin.site.register(MonthModel)
 admin.site.register(ReportModel)
 admin.site.register(ReportTypeModel)
 admin.site.register(GeneralDrilldownJSONDataModel)
+admin.site.register(TableNameModel)
+admin.site.register(ReportStorageModel)
