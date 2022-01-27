6aab5633de93dea00f888c7367761820db40bcb0
shahid <shahid.ice.nstu@gmail.com>
2022-01-27 14:52:30 +0600

    add django_extensions app

--- src/settings.py
+++ src/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third party app
     'rest_framework',
+    'django_extensions',
     # local apps
     'monthly_report.apps.MonthlyReportConfig',
     'users.apps.UsersConfig',
