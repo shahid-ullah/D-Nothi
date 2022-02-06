27552668b9eca35dd5725c4156d71275652e132c
shahid <shahid.ice.nstu@gmail.com>
2022-02-06 16:06:29 +0600

    Add report: nothi users total

--- dashboard_generate/urls.py
+++ dashboard_generate/urls.py
@@ -6,8 +6,13 @@ urlpatterns = [
     path('', views.total_offices_view, name="total_offices"),
     path('', views.total_offices_view, name="home"),
     path(
-        'nispottikritto_nothi',
+        'nispottikritto_nothi/',
         views.nispottikritto_nothi_view,
         name="nispottikritto_nothi",
+    ),
+    path(
+        'nothi_users_total/',
+        views.nothi_users_total,
+        name="nothi_users_total",
     ),
 ]
--- dashboard_generate/views.py
+++ dashboard_generate/views.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 from django.shortcuts import render
 
-from .models import ReportNispottikrittoNothiModel, ReportTotalOfficesModel
+from .models import (ReportNispottikrittoNothiModel, ReportTotalOfficesModel,
+                     ReportTotalUsersModel)
 
 
 class NpEncoder(json.JSONEncoder):
@@ -105,3 +106,32 @@ def nispottikritto_nothi_view(request):
     }
 
     return render(request, 'dashboard_generate/nispottikritto_nothi.html', context)
+
+
+def nothi_users_total(request):
+    objs = ReportTotalUsersModel.objects.all()
+    year_map, month_map, day_map = generate_year_month_and_day_map(objs)
+    # global nispottikritto_nothi_general_series, nispottikritto_nothi_drilldown_series
+    # if nispottikritto_nothi_general_series and nispottikritto_nothi_drilldown_series:
+    #     context = {
+    #         'general_series': json.dumps(
+    #             nispottikritto_nothi_general_series, cls=NpEncoder
+    #         ),
+    #         'drilldown_series': json.dumps(
+    #             nispottikritto_nothi_drilldown_series, cls=NpEncoder
+    #         ),
+    #     }
+    #     return render(request, 'monthly_report/nispottikritto_nothi.html', context)
+
+    # general_series, drilldown_series = load_nispottikritto_nothi_graph_data()
+
+    # nispottikritto_nothi_general_series = copy.deepcopy(general_series)
+    # nispottikritto_nothi_drilldown_series = copy.deepcopy(drilldown_series)
+
+    context = {
+        'year_map': json.dumps(year_map, cls=NpEncoder),
+        'month_map': json.dumps(month_map, cls=NpEncoder),
+        'day_map': json.dumps(day_map, cls=NpEncoder),
+    }
+
+    return render(request, 'dashboard_generate/nothi_users_total.html', context)
