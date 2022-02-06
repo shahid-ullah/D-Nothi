0c6029bfa0ff74e061de1c716b6f15552c67cd48
shahid <shahid.ice.nstu@gmail.com>
2022-02-06 10:59:23 +0600

    Add ReportNispottikrittoModel

--- dashboard_generate/admin.py
+++ dashboard_generate/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ReportTotalOfficesModel
+from .models import ReportNispottikrittoNothiModel, ReportTotalOfficesModel
 
 
 @admin.register(ReportTotalOfficesModel)
-class PostAdmin(admin.ModelAdmin):
+class ReportTotalOfficeModelAdmin(admin.ModelAdmin):
     list_display = [field.name for field in ReportTotalOfficesModel._meta.get_fields()]
+
+
+@admin.register(ReportNispottikrittoNothiModel)
+class ReportNispottikrittoNothiModelAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name for field in ReportNispottikrittoNothiModel._meta.get_fields()
+    ]
