5ad55b965c839cba8bd1d3e6017cb8fa88f682ad
shahid <shahid.ice.nstu@gmail.com>
2022-03-28 10:49:13 +0600

    refine android ios users automate script

--- automate_process/scripts/reports/android_ios_users.py
+++ automate_process/scripts/reports/android_ios_users.py
@@ -1,24 +1,91 @@
 # scripts/reports/android_ios_users.py
+from datetime import datetime
 
 import pandas as pd
 
 from dashboard_generate.models import (ReportAndroidUsersModel,
                                        ReportIOSUsersModel)
 
-from ..utils import utilsContainer
+
+def get_single_digit_maps():
+    map = {}
+    for i in range(0, 10):
+        value = f"0{i}"
+        map.setdefault(i, value)
+
+    return map
 
 
-def update(objs, request=None, *args, **kwargs):
+SINGLE_DIGIT_KEY_MAPS = get_single_digit_maps()
+
+
+def generate_year_month_day_key_and_report_date(year, month, day):
+    if month < 10:
+        month = SINGLE_DIGIT_KEY_MAPS[month]
+
+    if day < 10:
+        day = SINGLE_DIGIT_KEY_MAPS[day]
+    year_month_day = f"{year}{month}{day}"
+    report_date = f"{year}-{month}-{day}"
+
+    return year_month_day, report_date
+
+
+def generate_model_object_dictionary(request, year, month, day, count):
+    year_month_day, report_date = generate_year_month_day_key_and_report_date(
+        year, month, day)
+    model_object_dict = {
+        'year': year,
+        'month': month,
+        'day': day,
+        'count_or_sum': count,
+        'year_month_day': year_month_day,
+        'report_date': report_date,
+        'report_day': datetime(year, month, day)
+    }
+    try:
+        if request.user.is_authenticated:
+            model_object_dict['creator'] = request.user
+    except Exception as e:
+        pass
+
+    return model_object_dict
+
+
+def format_and_load_to_mysql_db(request, groupby_date, model):
+    last_report_date = ''
+
+    for date, frame in groupby_date:
+        last_report_date = date
+
+        count = frame['id'].count()
+
+        dict_ = generate_model_object_dictionary(request, date.year,
+                                                 date.month, date.day, count)
+        defaults = {'count_or_sum': count}
+
+        try:
+            obj = model.objects.get(year_month_day=dict_['year_month_day'])
+            for key, value in defaults.items():
+                setattr(obj, key, value)
+            obj.save()
+        except model.DoesNotExist:
+            obj = model(**dict_)
+            obj.save()
+
+    return last_report_date
+
+
+def update(dataframe, request=None, *args, **kwargs):
     status = {}
     status['android'] = {}
     status['ios'] = {}
     try:
-        utilsObject = utilsContainer()
         print()
         print('start processing android & ios users report')
 
-        values = objs.values('id', 'is_mobile', 'device_type', 'created')
-        dataframe = pd.DataFrame(values)
+        # values = objs.values('id', 'is_mobile', 'device_type', 'created')
+        # dataframe = pd.DataFrame(values)
 
         dataframe = dataframe.loc[dataframe.is_mobile == 1]
         # remove null values
@@ -28,12 +95,12 @@ def update(objs, request=None, *args, **kwargs):
         android_dataframe = dataframe.loc[dataframe.device_type == 'android']
         android_groupby_date = android_dataframe.groupby(
             android_dataframe.created.dt.date)
-        android_last_report_date = utilsObject.format_and_load_to_mysql_db(
+        android_last_report_date = format_and_load_to_mysql_db(
             request, android_groupby_date, ReportAndroidUsersModel)
 
         ios_dataframe = dataframe.loc[dataframe.device_type == 'IOS']
         ios_groupby_date = ios_dataframe.groupby(ios_dataframe.created.dt.date)
-        ios_last_report_date = utilsObject.format_and_load_to_mysql_db(
+        ios_last_report_date = format_and_load_to_mysql_db(
             request, ios_groupby_date, ReportIOSUsersModel)
 
         print()
