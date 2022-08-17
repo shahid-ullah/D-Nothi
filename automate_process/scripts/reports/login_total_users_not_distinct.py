afc58c73acfd07a6ce71ea19cea410d129b49ac8
shahid <shahid.ice.nstu@gmail.com>
2022-08-17 16:34:41 +0600

    add not distinct login report count

---
+++ automate_process/scripts/reports/login_total_users_not_distinct.py
@@ -0,0 +1,76 @@
+# scripts/reports/login_total_users.py
+# SELECT count(`employee_record_id`) FROM `user_login_history` WHERE
+# `created` >= '2022-01-01 00:00:00' AND `created` <= '2022-01-31 23:59:59'
+from datetime import datetime
+
+import pandas as pd
+
+from dashboard_generate.models import ReportLoginTotalUsersNotDistinct
+
+from . import utils
+
+
+def generate_model_object_dict(request, office_id, report_date, count_or_sum, *args, **kwargs):
+    object_dic = {}
+    object_dic['year'] = report_date.year
+    object_dic['month'] = report_date.month
+    object_dic['day'] = report_date.day
+    object_dic['report_date'] = datetime(report_date.year, report_date.month, report_date.day)
+    object_dic['counts'] = int(count_or_sum)
+    object_dic['office_id'] = int(office_id)
+
+    return object_dic
+
+def format_and_load_to_mysql_db(request, *args, **kwargs):
+    dataframe = kwargs['dataframe']
+    grouped = dataframe.groupby(['office_id', 'report_date'], as_index=False, sort=False)['id'].size()
+
+    batch_objects = []
+
+    for office_id, report_date, counts in zip(grouped['office_id'].values, grouped['report_date'].values, grouped['size'].values):
+
+        object_dic = generate_model_object_dict(request, office_id, report_date, counts)
+        batch_objects.append(ReportLoginTotalUsersNotDistinct(**object_dic))
+
+        if len(batch_objects) >= 100:
+            ReportLoginTotalUsersNotDistinct.objects.bulk_create(batch_objects)
+            batch_objects = []
+
+    ReportLoginTotalUsersNotDistinct.objects.bulk_create(batch_objects)
+
+    return None
+
+
+def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
+    querysets = kwargs.get('querysets')
+    querysets_values = querysets.values('id', 'created', 'office_id')
+    dataframe = pd.DataFrame(querysets_values)
+
+    # convert created object to datetime
+    dataframe['created'] = pd.to_datetime(dataframe['created'], errors='coerce')
+    dataframe = dataframe.loc[~dataframe['created'].isnull(), :]
+    dataframe = dataframe.loc[~dataframe['office_id'].isnull(), :]
+
+    # generate date column
+    dataframe['report_date'] = dataframe['created'].dt.date
+
+    if not dataframe.empty:
+        kwargs['dataframe'] = dataframe
+        format_and_load_to_mysql_db(request, *args, **kwargs)
+
+
+def generate_report(request=None, *args, **kwargs):
+    print()
+    print('start processing login_total_users not distinct report')
+
+    querysets = utils.get_user_login_history_querysets(request, *args, **kwargs)
+    querysets = querysets.exclude(created__isnull=True)
+
+    if querysets.exists():
+        kwargs['querysets'] = querysets
+        querysets_to_dataframe_and_refine(request, *args, **kwargs)
+
+    print('End processing login_total_users not distinct report')
+    print()
+
+    return None
