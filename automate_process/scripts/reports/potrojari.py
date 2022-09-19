624bffdba3699ea9bbfd7417ef7bdcd7a35fc020
shahid <shahid.ice.nstu@gmail.com>
2022-09-19 16:28:03 +0600

    store potrojari report as office and day wise

--- automate_process/scripts/reports/potrojari.py
+++ automate_process/scripts/reports/potrojari.py
@@ -4,12 +4,13 @@
 from datetime import datetime, timedelta
 
 import pandas as pd
+
 from automate_process.models import NisponnoRecords
-from backup_source_db.models import BackupDBLog, TrackBackupDBLastFetchTime
+from backup_source_db.models import BackupDBLog
 from dashboard_generate.models import ReportPotrojariModel
 
 
-def generate_model_object_dict(request, report_date, count_or_sum, *args, **kwargs):
+def generate_model_object_dict(request, report_date, count_or_sum, office_id, *args, **kwargs):
     object_dic = {}
     object_dic['year'] = report_date.year
     object_dic['month'] = report_date.month
@@ -18,6 +19,7 @@ def generate_model_object_dict(request, report_date, count_or_sum, *args, **kwar
     object_dic['report_date'] = str(report_date)
     object_dic['report_day'] = datetime(report_date.year, report_date.month, report_date.day)
     object_dic['count_or_sum'] = int(count_or_sum)
+    object_dic['office_id'] = int(office_id)
 
     return object_dic
 
@@ -25,27 +27,30 @@ def generate_model_object_dict(request, report_date, count_or_sum, *args, **kwar
 def format_and_load_to_mysql_db(request=None, *args, **kwargs):
 
     dataframe = kwargs['dataframe']
-    grouped_report_date = dataframe.groupby(['report_date'], sort=False, as_index=False)['id'].size()
+    grouped_report_date = dataframe.groupby(['office_id', 'report_date'], as_index=False, sort=False)['id'].size()
     batch_objects = []
 
-    for report_date, potrojari_count in zip(
-        grouped_report_date['report_date'].values, grouped_report_date['size'].values
+    for office_id, report_date, potrojari_count in zip(
+        grouped_report_date['office_id'].values,
+        grouped_report_date['report_date'].values,
+        grouped_report_date['size'].values,
     ):
-        object_dict = generate_model_object_dict(request, report_date, potrojari_count, *args, **kwargs)
+        object_dict = generate_model_object_dict(request, report_date, potrojari_count, office_id, *args, **kwargs)
         batch_objects.append(ReportPotrojariModel(**object_dict))
 
         if len(batch_objects) >= 100:
             ReportPotrojariModel.objects.bulk_create(batch_objects)
             batch_objects = []
 
-    ReportPotrojariModel.objects.bulk_create(batch_objects)
+    if batch_objects:
+        ReportPotrojariModel.objects.bulk_create(batch_objects)
 
     return None
 
 
 def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
     querysets = kwargs['querysets']
-    querysets_values = querysets.values('id', 'type', 'upokarvogi', 'operation_date')
+    querysets_values = querysets.values('id', 'office_id', 'type', 'upokarvogi', 'operation_date')
     dataframe = pd.DataFrame(querysets_values)
 
     # filter on potrojari
@@ -54,6 +59,7 @@ def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
     # convert operation_date object to datetime
     dataframe['operation_date'] = pd.to_datetime(dataframe.operation_date, errors='coerce')
     dataframe = dataframe.loc[~dataframe['operation_date'].isnull(), :]
+    dataframe = dataframe.loc[~dataframe['office_id'].isnull(), :]
 
     # generate date column
     dataframe['report_date'] = dataframe['operation_date'].dt.date
