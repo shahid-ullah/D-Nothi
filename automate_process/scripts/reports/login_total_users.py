a57cbc6fb99ad7cec33af4f776ea4b69d69c67cb
shahid <shahid.ice.nstu@gmail.com>
2022-09-19 11:13:27 +0600

    store login_total_users report as office and day wise

--- automate_process/scripts/reports/login_total_users.py
+++ automate_process/scripts/reports/login_total_users.py
@@ -2,14 +2,16 @@
 # SELECT count(DISTINCT(`employee_record_id`)) FROM `user_login_history` WHERE
 # `created` >= '2022-01-01 00:00:00' AND `created` <= '2022-01-31 23:59:59'
 from datetime import datetime, timedelta
+from tokenize import group
 
 import pandas as pd
+
 from automate_process.models import UserLoginHistory
 from backup_source_db.models import BackupDBLog, TrackBackupDBLastFetchTime
 from dashboard_generate.models import ReportLoginTotalUsers
 
 
-def generate_model_object_dict(request, report_date, count_or_sum, *args, **kwargs):
+def generate_model_object_dict(request, report_date, count_or_sum, office_id, *args, **kwargs):
     object_dic = {}
     object_dic['year'] = report_date.year
     object_dic['month'] = report_date.month
@@ -18,50 +20,47 @@ def generate_model_object_dict(request, report_date, count_or_sum, *args, **kwar
     object_dic['report_date'] = str(report_date)
     object_dic['report_day'] = datetime(report_date.year, report_date.month, report_date.day)
     object_dic['count_or_sum'] = int(count_or_sum)
+    object_dic['office_id'] = int(office_id)
 
     return object_dic
 
 
 def format_and_load_to_mysql_db(request, *args, **kwargs):
-    dataframe = kwargs['dataframe']
-    grouped_report_date = dataframe.groupby(['report_date'], sort=False, as_index=False)
+    df = kwargs['dataframe']
+    grouped_report_date = df.groupby(['office_id', 'report_date'], sort=False, as_index=True)[
+        'employee_record_id'
+    ].apply(set)
+    grouped_report_date = grouped_report_date.reset_index(name='employee_ids')
 
     batch_objects = []
-    for report_date, frame in grouped_report_date:
-        print('login_total_users report: ', str(report_date))
-
-        count_or_sum = int(frame['employee_record_id'].nunique())
-        object_dict = generate_model_object_dict(request, report_date, count_or_sum, *args, **kwargs)
-
-        employee_ids = {}
-
-        for employee_id in frame.employee_record_id.values:
-            employee_ids.setdefault(f'{employee_id}', 1)
-
+    for office_id, report_date, employee_ids in zip(
+        grouped_report_date['office_id'].values,
+        grouped_report_date['report_date'].values,
+        grouped_report_date['employee_ids'].values,
+    ):
+        employee_ids = {f"{id_}": 1 for id_ in employee_ids}
+        object_dict = generate_model_object_dict(request, report_date, len(employee_ids), office_id, *args, **kwargs)
         object_dict['employee_record_ids'] = employee_ids
         batch_objects.append(ReportLoginTotalUsers(**object_dict))
-
         if len(batch_objects) >= 100:
             ReportLoginTotalUsers.objects.bulk_create(batch_objects)
             batch_objects = []
 
-    try:
-        if batch_objects:
-            ReportLoginTotalUsers.objects.bulk_create(batch_objects)
-    except Exception as e:
-        print(e)
+    if batch_objects:
+        ReportLoginTotalUsers.objects.bulk_create(batch_objects)
 
     return None
 
 
 def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
     querysets = kwargs.get('querysets')
-    querysets_values = querysets.values('employee_record_id', 'created')
+    querysets_values = querysets.values('office_id', 'employee_record_id', 'created')
     dataframe = pd.DataFrame(querysets_values)
 
     # convert created object to datetime
     dataframe['created'] = pd.to_datetime(dataframe['created'], errors='coerce')
     dataframe = dataframe.loc[~dataframe['created'].isnull(), :]
+    dataframe = dataframe.loc[~dataframe['office_id'].isnull(), :]
 
     # generate date column
     dataframe['report_date'] = dataframe['created'].dt.date
