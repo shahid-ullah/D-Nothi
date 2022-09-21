bf75cb82ca51b7fb9e6731280c83e21c0bc391e8
shahid <shahid.ice.nstu@gmail.com>
2022-09-21 16:08:14 +0600

    update login_total_users report generation

--- automate_process/scripts/reports/login_total_users.py
+++ automate_process/scripts/reports/login_total_users.py
@@ -5,10 +5,9 @@ from datetime import datetime, timedelta
 from tokenize import group
 
 import pandas as pd
-
 from automate_process.models import UserLoginHistory
 from backup_source_db.models import BackupDBLog, TrackBackupDBLastFetchTime
-from dashboard_generate.models import ReportLoginTotalUsers
+from dashboard_generate.models import ReportGenerationLog, ReportLoginTotalUsers
 
 
 def generate_model_object_dict(request, report_date, count_or_sum, office_id, *args, **kwargs):
@@ -71,22 +70,17 @@ def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
 
 
 def get_user_login_history_querysets(*args, **kwargs):
+    querysets = kwargs['querysets']
+
+    if querysets is not None:
+        return querysets
+
     querysets = UserLoginHistory.objects.using('source_db').all()
-    backup_log = BackupDBLog.objects.using('backup_source_db').last()
 
     if ReportLoginTotalUsers.objects.exists():
-        try:
-            last_login_history_time = backup_log.last_login_history_time
-            if last_login_history_time:
-                querysets = querysets.filter(created__gt=last_login_history_time)
-            else:
-                last_fetch_time = ReportLoginTotalUsers.objects.last().report_day
-                last_fetch_time = last_fetch_time + timedelta(days=1)
-                querysets = querysets.filter(created__gte=last_fetch_time)
-        except AttributeError:
-            last_fetch_time = ReportLoginTotalUsers.objects.last().report_day
-            last_fetch_time = last_fetch_time + timedelta(days=1)
-            querysets = querysets.filter(created__gte=last_fetch_time)
+        last_fetch_time = ReportLoginTotalUsers.objects.last().report_day
+        last_fetch_time = last_fetch_time + timedelta(days=1)
+        querysets = querysets.filter(created__gte=last_fetch_time)
 
     return querysets
 
