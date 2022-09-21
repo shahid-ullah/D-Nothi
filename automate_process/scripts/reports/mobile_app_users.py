b86e5d3352b9be4b62efc8b2bdb794ce815e7f80
shahid <shahid.ice.nstu@gmail.com>
2022-09-21 16:03:35 +0600

    update mobile app users report generation

--- automate_process/scripts/reports/mobile_app_users.py
+++ automate_process/scripts/reports/mobile_app_users.py
@@ -6,9 +6,8 @@
 from datetime import datetime, timedelta
 
 import pandas as pd
-
 from backup_source_db.models import BackupDBLog
-from dashboard_generate.models import ReportMobileAppUsersModel
+from dashboard_generate.models import ReportGenerationLog, ReportMobileAppUsersModel
 
 from ...models import UserLoginHistory
 
@@ -75,22 +74,17 @@ def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
 
 
 def get_user_login_history_querysets(*args, **kwargs):
+    querysets = kwargs['querysets']
+
+    if querysets is not None:
+        return querysets
+
     querysets = UserLoginHistory.objects.using('source_db').all()
-    backup_log = BackupDBLog.objects.using('backup_source_db').last()
 
     if ReportMobileAppUsersModel.objects.exists():
-        try:
-            last_login_history_time = backup_log.last_login_history_time
-            if last_login_history_time:
-                querysets = querysets.filter(created__gt=last_login_history_time)
-            else:
-                last_fetch_time = ReportMobileAppUsersModel.objects.last().report_day
-                last_fetch_time = last_fetch_time + timedelta(days=1)
-                querysets = querysets.filter(created__gte=last_fetch_time)
-        except AttributeError:
-            last_fetch_time = ReportMobileAppUsersModel.objects.last().report_day
-            last_fetch_time = last_fetch_time + timedelta(days=1)
-            querysets = querysets.filter(created__gte=last_fetch_time)
+        last_fetch_time = ReportMobileAppUsersModel.objects.last().report_day
+        last_fetch_time = last_fetch_time + timedelta(days=1)
+        querysets = querysets.filter(created__gte=last_fetch_time)
 
     return querysets
 
