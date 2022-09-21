62f2f6a531f3057dc871d9b5acaede760f9d3f70
shahid <shahid.ice.nstu@gmail.com>
2022-09-21 15:56:55 +0600

    update potrojari report generation

--- automate_process/scripts/reports/potrojari.py
+++ automate_process/scripts/reports/potrojari.py
@@ -4,10 +4,9 @@
 from datetime import datetime, timedelta
 
 import pandas as pd
-
 from automate_process.models import NisponnoRecords
 from backup_source_db.models import BackupDBLog
-from dashboard_generate.models import ReportPotrojariModel
+from dashboard_generate.models import ReportGenerationLog, ReportPotrojariModel
 
 
 def generate_model_object_dict(request, report_date, count_or_sum, office_id, *args, **kwargs):
@@ -72,17 +71,14 @@ def querysets_to_dataframe_and_refine(request=None, *args, **kwargs):
 
 
 def get_nisponno_records_querysets(*args, **kwargs):
+    querysets = kwargs['querysets']
+
+    if querysets is not None:
+        return querysets
+
     querysets = NisponnoRecords.objects.using('source_db').all()
-    backup_log = BackupDBLog.objects.using('backup_source_db').last()
-    try:
-        last_nisponno_records_time = backup_log.last_nisponno_records_time
-        if last_nisponno_records_time:
-            querysets = querysets.filter(created__gt=last_nisponno_records_time)
-        else:
-            last_fetch_time = ReportPotrojariModel.objects.last().report_day
-            last_fetch_time = last_fetch_time + timedelta(days=1)
-            querysets = querysets.filter(created__gte=last_fetch_time)
-    except AttributeError:
+
+    if ReportPotrojariModel.objects.exists():
         last_fetch_time = ReportPotrojariModel.objects.last().report_day
         last_fetch_time = last_fetch_time + timedelta(days=1)
         querysets = querysets.filter(created__gte=last_fetch_time)
