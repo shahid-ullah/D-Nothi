bfade683730225c73f19401122bf29e2c32c893a
shahid <shahid.ice.nstu@gmail.com>
2022-09-22 15:46:26 +0600

    comment out scripts.py

--- scripts.py
+++ scripts.py
@@ -1,33 +1,33 @@
-from django.core.paginator import Paginator
+# from django.core.paginator import Paginator
 
-from automate_process.models import UserLoginHistory
-from backup_source_db.models import BackupUserLoginHistory
-
-
-def backup_login_history_table():
-    querysets = UserLoginHistory.objects.using('source_db').all()
-    fields = [f.name for f in UserLoginHistory._meta.get_fields()]
-    fields.remove('id')
+# from automate_process.models import UserLoginHistory
+# from backup_source_db.models import BackupUserLoginHistory
 
 
-    print('pagination running')
-    paginator = Paginator(querysets, 10000)
-    total_page = paginator.num_pages
-    page_range = paginator.page_range
-    print('pagination complete')
+# def backup_login_history_table():
+#     querysets = UserLoginHistory.objects.using('source_db').all()
+#     fields = [f.name for f in UserLoginHistory._meta.get_fields()]
+#     fields.remove('id')
 
-    for page_number in page_range:
-        objs = paginator.page(page_number).object_list
-        print(f'offices: processing page {page_number} of {total_page}')
-        values = objs.values(*fields)
-        batch_objects = []
 
-        # batch_objects = [BackupUserLoginHistory(**row) for row in values]
-        # batch_objects = [row for row in values]
-        batch_objects = [j for j in len(values)]
-        # for row in values:
-        #     pass
-        #     row.pop('id')
-        #     last_fetch_time = row['created']
-            # batch_objects.append(BackupUserLoginHistory(**row))
-        # BackupUserLoginHistory.objects.using('backup_source_db').bulk_create(batch_objects)
+#     print('pagination running')
+#     paginator = Paginator(querysets, 10000)
+#     total_page = paginator.num_pages
+#     page_range = paginator.page_range
+#     print('pagination complete')
+
+#     for page_number in page_range:
+#         objs = paginator.page(page_number).object_list
+#         print(f'offices: processing page {page_number} of {total_page}')
+#         values = objs.values(*fields)
+#         batch_objects = []
+
+#         # batch_objects = [BackupUserLoginHistory(**row) for row in values]
+#         # batch_objects = [row for row in values]
+#         batch_objects = [j for j in len(values)]
+#         # for row in values:
+#         #     pass
+#         #     row.pop('id')
+#         #     last_fetch_time = row['created']
+#             # batch_objects.append(BackupUserLoginHistory(**row))
+#         # BackupUserLoginHistory.objects.using('backup_source_db').bulk_create(batch_objects)
